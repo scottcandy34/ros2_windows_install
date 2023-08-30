e7683df4467092c7a69d9c2701dd38e2c204d054
scottcandy34 <bzbrandonz@gmail.com>
2023-08-30 17:03:47 -0400

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,4 +1,6 @@
-﻿$code = & cmd /c '"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat" && set'
+$env:ChocolateyInstall = "C:\dev\chocolatey"
+$env:Path = "C:\dev\chocolatey\bin;" + $env:Path
+$code = & cmd /c '"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat" && set'
 $environment = $code | Select-String -Pattern '(.*)=(.*)'
 for($i=0; $i -lt $environment.Matches.Length; $i++) {
     $_name = $environment.Matches[$i].Groups[1].Value
